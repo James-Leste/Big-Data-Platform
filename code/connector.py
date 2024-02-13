61cd87bbd4bf58f2070c06b20ff67dc8b9e408ff
Ziqi Wang <wangziqi2385823041@outlook.com>
2024-02-13 16:21:04 +0200

    finish connector.py, add initDatabase()

--- code/connector.py
+++ code/connector.py
@@ -5,5 +5,33 @@ def initConnection(keyspace, addresses=[ADDRESSES,], port=9042):
     cluster = Cluster(addresses, port=port)
     session = cluster.connect(keyspace=keyspace)
     print("Connecting...")
-    print("Connected successfully to " + session + "!")
+    print("Connected successfully !")
     return session
+
+def initDatabase(keyspace="amazon"):
+    session = initConnection(keyspace)
+    session.execute("CREATE TABLE IF NOT EXISTS reviews_by_id (\
+        marketplace text,\
+        customer_id int,\
+        review_id text,\
+        product_id text,\
+        product_parent text,\
+        product_title text,\
+        product_category text, \
+        star_rating int,\
+        helpful_votes int,\
+        total_votes int,\
+        verified_purchase text,\
+        review_headline text,\
+        review_body text,\
+        review_date text,\
+        PRIMARY KEY (review_id)\
+    );")
+    print("creating database table [reviews]")
+    session.shutdown()
+
+def main():
+    pass
+    
+if __name__ == "__main__":
+    main()
