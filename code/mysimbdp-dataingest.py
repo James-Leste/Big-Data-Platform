14637b46e3f1f7674d571ad9a9f9ba46a0a03551
Ziqi Wang <wangziqi2385823041@outlook.com>
2024-02-13 21:32:45 +0200

    injesting simple data successful

--- code/mysimbdp-dataingest.py
+++ code/mysimbdp-dataingest.py
@@ -1,20 +1,54 @@
 import pandas as pd
 import numpy as np
-import json
+from connector import initConnection, initDatabase
+from cassandra.query import BatchStatement
+from cassandra import ConsistencyLevel
+from cassandra.policies import FallthroughRetryPolicy
+
 FILEPATH1 = "/Users/jamesroot/AAAroot/Course notes/Aalto Big Data Platforms/assignment-1-101699682/test/amazon_reviews_us_Digital_Software_v1_00.tsv"
 FILEPATH2 = "/Users/jamesroot/AAAroot/Course notes/Aalto Big Data Platforms/assignment-1-101699682/test/amazon_reviews_us_Gift_Card_v1_00.tsv"
-def dataInit(filePath):
-    df = pd.read_csv(filepath_or_buffer=filePath, sep="\t", )
-    print(df.info())
-    x = df.head().to_json()
-    y = json.loads(x)
-    return y
+TESTFILEPATH = "/Users/jamesroot/AAAroot/Course notes/Aalto Big Data Platforms/assignment-1-101699682/test/test.tsv"
 
+INSERT = "INSERT INTO reviews_by_id \
+        (marketplace, customer_id, review_id, \
+        product_id, product_parent, product_title, \
+        product_category, star_rating, helpful_votes, \
+        total_votes, verified_purchase, review_headline, \
+        review_body, review_date) \
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
 
+def injesting(file, batchsize=3, delimiter="\t"):
+    initDatabase("test")
+    session = initConnection("test")
+    insert_stmt = session.prepare(INSERT)
+    reader = pd.read_csv(file, chunksize=batchsize, delimiter=delimiter)
+    batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM, retry_policy=FallthroughRetryPolicy())
+
+    try:
+        while True:
+            df = next(reader)
+            for i, s in df.iterrows():
+                batch.add(insert_stmt, [s.get("marketplace"), s.get("customer_id"), 
+                                              s.get("review_id"), s.get("product_id"),
+                                              s.get("product_parent"), s.get("product_title"), 
+                                              s.get("product_category"), s.get("star_rating"), 
+                                              s.get("helpful_votes"), s.get("total_votes"), 
+                                              s.get("verified_purchase"), s.get("review_headline"), 
+                                              s.get("review_body"), s.get("review_date")])
+                
+                #print("Batch added: " + str(realbatchsize))
+            session.execute(batch)
+            #print("Batch written: " + str(realbatchsize))
+            #print("------")
+            batch.clear()
+    except StopIteration:
+        #print("total " + str(total) + " records added.")
+        print("Reached end of file.")
+    
+    session.shutdown()
 
 def main():
-    j = dataInit(FILEPATH2)
-    print(j)
+    injesting(FILEPATH2)
     
 
 main()
