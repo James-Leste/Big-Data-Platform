3e53dce02f6bf146d57e3da756a1664d470589c5
Ziqi Wang <wangziqi2385823041@outlook.com>
2024-02-15 10:08:33 +0200

    complete datainjesting

--- code/mysimbdp-dataingest.py
+++ code/mysimbdp-dataingest.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from connector import initConnection, initDatabase
-from cassandra.query import BatchStatement
-from cassandra import ConsistencyLevel
-from cassandra.policies import FallthroughRetryPolicy
+# from cassandra.query import BatchStatement
+# from cassandra import ConsistencyLevel
+# from cassandra.policies import FallthroughRetryPolicy
 import logging
+# from datetime import datetime
 
 FILEPATH1 = "/Users/jamesroot/AAAroot/Course notes/Aalto Big Data Platforms/assignment-1-101699682/test/amazon_reviews_us_Digital_Software_v1_00.tsv"
 FILEPATH2 = "/Users/jamesroot/AAAroot/Course notes/Aalto Big Data Platforms/assignment-1-101699682/test/amazon_reviews_us_Gift_Card_v1_00.tsv"
@@ -17,14 +18,15 @@ INSERT = "INSERT INTO reviews_by_id \
         review_body, review_date) \
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
 
-logging.basicConfig(filename='data_ingestion_errors.log', level=logging.ERROR, format='%(asctime)s:%(levelname)s:%(message)s')
+logging.basicConfig(filename='data_ingestion_errors.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
 
-def injesting(file, batchsize=3, delimiter="\t"):
-    initDatabase("test")
-    session = initConnection("test")
+
+def injesting(file, address, port, batchsize=3, delimiter="\t"):
+    initDatabase("test", addresses=address, port=port)
+    session = initConnection("test", addresses=address, port=port)
     insert_stmt = session.prepare(INSERT)
     reader = pd.read_csv(file, chunksize=batchsize, delimiter=delimiter)
-    batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM, retry_policy=FallthroughRetryPolicy())
+    #batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM, retry_policy=FallthroughRetryPolicy())
 
     total = 0
     try:
@@ -32,7 +34,7 @@ def injesting(file, batchsize=3, delimiter="\t"):
             df = next(reader)
             for i, s in df.iterrows():
                 try:
-                    batch.add(insert_stmt, [s.get("marketplace"), s.get("customer_id"), 
+                    session.execute_async(insert_stmt, [s.get("marketplace"), s.get("customer_id"), 
                                                 s.get("review_id"), s.get("product_id"),
                                                 s.get("product_parent"), s.get("product_title"), 
                                                 s.get("product_category"), s.get("star_rating"), 
@@ -44,17 +46,25 @@ def injesting(file, batchsize=3, delimiter="\t"):
                     # Log the error and the problematic row for review
                     logging.error(f"Error processing row {i}: {e}")
                     logging.error(f"Problematic data: {s.to_dict()}")
+                    logging.info("-----------------")
                     continue  # Skip the current row and continue with the next one
-            session.execute(batch)
-            batch.clear()
+                except TypeError as e:
+                    logging.error(f"Error processing row {i}: {e}")
+                    logging.error(f"Problematic data: {s.to_dict()}")
+                    logging.info("-----------------")
+                    continue  # Skip the current row and continue with the next one
+            #session.execute(batch)
+            #batch.clear()
     except StopIteration:
-        logging.info(f"Total {total} records added successfully.")
         logging.info("Reached end of file.")
-    
+        logging.info("-----------------")
+    finally:
+        logging.info(f"Total {total} records added successfully.")
+        logging.info("-----------------")
     session.shutdown()
 
 def main():
-    injesting(FILEPATH2)
+    injesting(FILEPATH2, address=["35.204.192.40",], port=9042)
     
 
 main()
